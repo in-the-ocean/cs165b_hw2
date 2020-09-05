3761fc3feed2aa701497c72f62ab1a7efd25426d
Stella Zhao <mengjia@ucsb.edu>
2020-09-04 22:39:26 -0700

    finalize

--- hw2.py
+++ hw2.py
@@ -5,15 +5,35 @@ import numpy as np
 from time import time
 from collections import Counter
 
+SETOSA = "Iris-setosa"
+
 class Data:
     def __init__(self):
         self.features = []	# list of lists (size: number_of_examples x number_of_features)
         self.labels = []	# list of strings (lenght: number_of_examples)
 
+    def addSample(self, sample):
+        sample = sample.strip()
+        feature = sample.split(",")
+        for i in range(4):
+            feature[i] = float(feature[i])
+        label = feature[-1]
+
+        if label == SETOSA:
+            l = 1
+        else:
+            l = -1
+        self.features.append(feature[0:4] + [1])
+        self.labels.append(l)
+
+
 def read_data(path):
     data = Data()
     # TODO: function that will read the input file and store it in the data structure
     # use the Data class defined above to store the information
+    with open(path) as f:
+        for line in f:
+            data.addSample(line)
     return data
 
 def dot_kf(u, v):
@@ -28,7 +48,10 @@ def dot_kf(u, v):
         u*v
     """
     # TODO: implement the kernel function
-    return
+    product = 0
+    for i in range(len(u)):
+        product += u[i] * v[i]
+    return product
 
 def poly_kernel(d):
     """
@@ -43,7 +66,7 @@ def poly_kernel(d):
     """
     def kf(u, v):
         # TODO: implement the kernel function
-        return 
+        return pow(dot_kf(u,v) + 1, d)
     return kf
 
 def exp_kernel(s):
@@ -59,7 +82,10 @@ def exp_kernel(s):
     """
     def kf(u, v):
         # TODO: implement the kernel function
-        return
+        dist = 0
+        for i in range(len(u)):
+            dist += (u[i] - v[i]) ** 2
+        return math.exp(-(dist/2*s*s))
     return kf
 
 class Perceptron:
@@ -71,12 +97,25 @@ class Perceptron:
         """
         self.MissedPoints = []
         self.MissedLabels = []
+        self.w = []
         self.kf = kf
         self.lr = lr
 
     def train(self, data):
         # TODO: Main function - train the perceptron with data
-        
+        converged = False
+        while not converged:
+            converged = True
+            for p in range(len(data.features)):
+                if self.update(data.features[p], data.labels[p]):
+                    self.MissedLabels.append(data.labels[p])
+                    self.MissedPoints.append(data.features[p])
+                    converged = False
+        self.w = [0 for i in range(5)]
+        for i in range(len(self.MissedLabels)):
+            for j in range(5):
+                self.w[j] += self.MissedPoints[i][j] * self.MissedLabels[i]
+
 
     def update(self, point, label):
         """
@@ -91,23 +130,47 @@ class Perceptron:
             False otherwise (prediction is accurate).
         """
         # TODO
-        return is_mistake
+        score = 0
+        for i in range(len(self.MissedPoints)):
+            score += self.kf(self.MissedPoints[i], point) * self.MissedLabels[i]
+        score *= label
+
+        return score <= 0 
 
     def predict(self, point):
         """
         Given a point, predicts the label of that point (1 or -1).
         """
         # TODO
-        return
+        score = 0
+        for i in range(len(self.MissedPoints)):
+            score += self.kf(self.MissedPoints[i], point) * self.MissedLabels[i]
+        print(score)
+        if score >= 0:
+            return 1
+        else:
+            return -1
 
     def test(self, data):
         predictions = []
         # TODO: given data and a perceptron - return a list of integers (+1 or -1).
         # +1 means it is Iris Setosa
         # -1 means it is not Iris Setosa
+        for d in data.features:
+            predictions.append(self.predict(d))
         return predictions
 
 
 # Feel free to add any helper functions as needed.
 if __name__ == '__main__':
-    
+    train = read_data("hw2_train.txt")
+    test = read_data("hw2_test.txt")
+    perceptron = Perceptron(exp_kernel(1), 0.5)
+    perceptron.train(train)
+    predict = perceptron.test(test)
+
+    correct = 0
+    for i in range(len(predict)):
+        if predict[i] == test.labels[i]:
+            correct += 1
+    print(correct/len(predict))
